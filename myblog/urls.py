f75d8998a30225faa1cff2945e46f8bde45b935a
vossisboss <vossmk@gmail.com>
2024-08-27 16:44:57 -0400

    Removed unnecessary i18n settings.

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.urls import include, path
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
 
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail import urls as wagtail_urls
@@ -9,11 +8,11 @@ from wagtail.documents import urls as wagtaildocs_urls
 
 from search import views as search_views
 
-# These paths are non-translatable so will not be given a language prefix
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
+    path("search/", search_views.search, name="search"),
 ]
 
 
@@ -25,9 +24,7 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# These paths are translatable so will be given a language prefix (eg, '/en', '/fr')
-urlpatterns = urlpatterns + i18n_patterns(
-    path("search/", search_views.search, name="search"),
+urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
     # the list:
@@ -35,4 +32,4 @@ urlpatterns = urlpatterns + i18n_patterns(
     # Alternatively, if you want Wagtail pages to be served from a subpath
     # of your site, rather than the site root:
     #    path("pages/", include(wagtail_urls)),
-)
+]
